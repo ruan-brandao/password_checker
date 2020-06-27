585136141e7ceac009d345e5d949b3f3be53d003
Ruan Brandão <ruan.bernardo@gmail.com>
2020-06-26 23:20:35 -0300

    Initial commit

---
+++ test/password_checker_web/views/error_view_test.exs
@@ -0,0 +1,15 @@
+defmodule PasswordCheckerWeb.ErrorViewTest do
+  use PasswordCheckerWeb.ConnCase, async: true
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test "renders 404.json" do
+    assert render(PasswordCheckerWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500.json" do
+    assert render(PasswordCheckerWeb.ErrorView, "500.json", []) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
